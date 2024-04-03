0cb9eb47fb1bad8b16bb46b7d3880b32fbda955b
arnauitb <arnau.molano.7e7@itb.cat>
2024-04-03 12:58:12 +0100

    Add project files.

---
+++ Gestor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC2TractamentDeFitxers
+{
+    public class Gestor
+    {
+        public int Any {  get; set; }
+        public int CodiComarca { get; set; }
+        public string Comarca { get; set; }
+        public int Poblacio { get; set; }
+        public  int DomesticXarxa { get; set; }
+        public int ActiEconom { get; set; }
+        public int Total {  get; set; }
+        public double ConsDomCap {  get; set; }
+
+    }
+
+    public static List<Gestor> CargarDatos(string filePath)
+    {
+        var datos = new List<Gestor>();
+
+        using (var reader = new StreamReader(filePath))
+        {
+            reader.ReadLine();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                datos.Add(new Gestor
+                {
+                    Any = int.Parse(values[0]),
+                    CodiComarca = int.Parse(values[1]),
+                    Comarca = values[2]
+
+                });
+
+
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using CsvHelper;
+using System;
+using System.Security.Cryptography.X509Certificates;
+namespace AC2TractamentDeFitxers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            using var reader = new StreamReader("ConsAigua.csv");
+            using var csv = new CsvReader (reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<Gestor>();
+
+            foreach (var record in records)
+            {
+                Console.WriteLine (record);
+            }
+
+           
+        }
+
+        public void PoblacioSuperior2000()
+        {
+
+        }
+    }
+}
